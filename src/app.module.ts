88d6f3aeeb000f63607a8f2f8fbf6b8842f89a2a
Sebastian Ruiz <sebasruiz909@gmail.com>
2022-10-21 14:29:26 -0500

    UPDATE : .env configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ConfigModule } from '@nestjs/config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     //yarn add @nestjs/serve-static
     // to render static file in the browser
     ServeStaticModule.forRoot({
@@ -16,11 +17,15 @@ import { SeedModule } from './seed/seed.module';
     }),
 
     //create reference to database
-    MongooseModule.forRoot('mongodb://localhost/pokemon'),
+    MongooseModule.forRoot(process.env.DB),
     // import modules
     PokemonModule,
     CommonModule,
     SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -50,4 +50,3 @@ export class PokemonController {
     return this.pokemonService.remove(id);
   }
 }
-
